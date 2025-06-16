fe81b74b25a2570a7d34ef9328407c2b6db4449c
Ahmad Haikal <haikala0010@gmail.com>
2025-06-16 08:11:30 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_pegawai_dosen_api
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_pegawai_dosen_api/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_pegawai_dosen_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
